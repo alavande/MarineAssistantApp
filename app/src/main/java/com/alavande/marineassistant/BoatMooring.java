f1e596c512021650afa259e01653cd1c3cf330d2
alavande <xiayanqiuqing@gmail.com>
2017-04-02 19:58:36 +1000

    new class

---
+++ app/src/main/java/com/alavande/marineassistant/BoatMooring.java
@@ -0,0 +1,39 @@
+package com.alavande.marineassistant;
+
+/**
+ * Created by hasee on 2017/04/02.
+ */
+
+public class BoatMooring {
+
+    private double longitude, latitude;
+    private String name, location, type;
+
+    public BoatMooring(double longitude, double latitude, String name, String location, String type) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.name = name;
+        this.location = location;
+        this.type = type;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
