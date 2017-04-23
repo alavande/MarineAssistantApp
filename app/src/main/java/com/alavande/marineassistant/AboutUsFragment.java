6d30389f4bb7e349049d0ef90c0f3ba19504d05b
alavande <xiayanqiuqing@gmail.com>
2017-04-23 20:05:40 +1000

    code changed

--- app/src/main/java/com/alavande/marineassistant/AboutUsFragment.java
+++ app/src/main/java/com/alavande/marineassistant/AboutUsFragment.java
@@ -3,9 +3,13 @@ package com.alavande.marineassistant;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 
 /**
@@ -13,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class AboutUsFragment extends Fragment {
 
+
+    private CardView aboutUsCard;
+    private TextView aboutUsText;
 
     public AboutUsFragment() {
         // Required empty public constructor
@@ -24,8 +31,14 @@ public class AboutUsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_about_us, container, false);
+        aboutUsCard = (CardView) view.findViewById(R.id.about_us_card);
 
-        
+        ExpandableTextView expTv1 = (ExpandableTextView) view.findViewById(R.id.expand_text_view);
+        expTv1.setText(getString(R.string.dummy_text1));
+
+        ExpandableTextView expTv2 = (ExpandableTextView) view.findViewById(R.id.expand_text_view1);
+        expTv2.setText(getString(R.string.dummy_text2));
+
         return view;
     }
 
